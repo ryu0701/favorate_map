29123b4302f8ec82edbd851cc8bfc9124d8ed086
ryu0701 <xrpadyju.268@icloud.com>
2023-04-01 13:26:17 +0900

    環境構築中

--- api_favorate_map/api/models.py
+++ api_favorate_map/api/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
 
 # Create your models here.
+
+"""
+BaseUserManagerをオーバーライド
+username,password => email,password
+"""
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None) -> object:
+        if not email:
+            raise ValueError('email is must')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password) -> object:
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+"""
+Userテーブル定義
+ユーザー情報テーブル
+"""
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # デフォルトだとusernameになってる
+
+    def __str__(self) -> str:
+        return self.email
+
+
+"""
+Profileテーブル定義
+プロフィールデータテーブル
+---------------------------
+One to one (User=>Profile)
+"""
+
+
+class Profile(models.Model):
+
+    nickName = models.CharField(max_length=20)
+    userProfile = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name='userProfile',
+        on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to=upload_avatar_path
+    )
+
+    def __str__(self) -> str:
+        return self.nickName
--- api_favorate_map/api_favorate_map/settings.py
+++ api_favorate_map/api_favorate_map/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'api_favorate_map.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'favorate_map_db',
+        'USER': 'arai.ryunosuke.xp',
+        'PASSWORD': 'P@ss_ara00',
+        'HOST': 'database-1.clpxgggbmo5y.ap-northeast-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
